ed99f04f8baba69e9379056abe3c25c80ff690a4
IDIRYACINE <idiryacinesp@gmail.com>
2022-01-08 17:18:38 +0100

    implementing storage upload to cloud (not done)

--- server/src/Storage/StorageService.js
+++ server/src/Storage/StorageService.js
@@ -1,10 +1,25 @@
-import { getStorage } from "firebase/storage";
+import { getStorage,ref,getDownloadURL,uploadBytes} from "firebase/storage";
 
 class StorageService{
     #cloudStorage;
-    
+
     constructor(firebaseApp){
         this.#cloudStorage = getStorage(firebaseApp) ;
+    }
+
+    async UploadFile(file){
+        const storageRef = ref(storage, 'some-child');
+        uploadBytes(storageRef, file)
+    }
+
+    async DonwloadFile(downloadUrl){
+        const file = ref(this.#cloudStorage , downloadUrl)
+        getDownloadURL(file).then((url) => {
+           return url
+          })
+          .catch((error) => {
+            return error
+          });
     }
 }
 
