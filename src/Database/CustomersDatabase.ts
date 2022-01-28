5f0f6259b5631606015af5ee80f2cf62be392e9c
IDIRYACINE <idiryacinesp@gmail.com>
2022-01-28 18:00:41 +0100

    migrated from sqlite3 to better-sqlite3 package

--- src/Database/CustomersDatabase.ts
+++ src/Database/CustomersDatabase.ts
@@ -1,158 +1,152 @@
-import sqlite3 from 'sqlite3'
-import {open ,Database} from 'sqlite'
-import AttributeHolder from './Repository/AttributeHolder'
-
-class CustomersDatabase{
-    
-    #configurations = {
-        databaseName : "customers.db",
-        databaseUrl : "./data",
-        mainTable : "Customers",
-        mainTableAttrbs : ["FullName","Email","PhoneNumber","BanStatus"],
-        secondaryTable : "Extras",
-        secondaryTableAttrbs : ["Rating","NegativeRating","Latitude","Longitude","Addresse"]
-    }
-    
-    #connection : Database
-
-   
-    async connect(){
-        this.#connection = await open({
-            filename: this.#configurations.databaseUrl + '/' +this.#configurations.databaseName,
-            driver: sqlite3.Database
-        })
-
-        const create_customers_table_query = "CREATE TABLE IF NOT EXISTS "+ this.#configurations.mainTable +" (\n"
-        + "	Id String PRIMARY KEY,"
-        + "	FullName text NOT NULL,"
-        + "	Email text NOT NULL,"
-        + " PhoneNumber text NOT NULL,"
-        + " BanStatus bool DEFAULT true "
-        + ")"
-
-        const create_customers_extraInfos_table_query = "CREATE TABLE IF NOT EXISTS "+ this.#configurations.secondaryTable +" ("
-        + "	Id String PRIMARY KEY,"
-        + " Rating real DEFAULT 0,"
-        + " NegativeRating real DEFAULT 0,"
-        + " Latitude real ,"
-        + " Longitude real ,"
-        + " Address text ,"
-        + ")"
-
-        this.#connection.exec(create_customers_table_query)
-        this.#connection.exec(create_customers_extraInfos_table_query)
-    }
-
-    async RegsiterCustomer(customer : any){
-        let register_customer_query = "INSERT INTO "+ this.#configurations.mainTable
-            +this.#InsertValuesQueryHelper(customer , customer.length)
-
-        this.#connection.exec(register_customer_query)
-    }
-
-    async RegisterCustomerExtras(extras : any){
-        let register_customer_extras_query = "INSERT INTO "+ this.#configurations.secondaryTable
-        + this.#InsertValuesQueryHelper(extras,extras.length) 
-
-        this.#connection.exec(register_customer_extras_query)
-    }
-
-    async UpdateCustomer(customer : any){
-        let update_customer_query = "UPDATE "+ this.#configurations.mainTable
-            +this.#UpdateValuesQueryHelper(customer , customer.length)
-            
-
-        this.#connection.exec(update_customer_query)
-    }
-
-    async UpdateCustomerExtras(extras : any){
-        let update_customer_extras_query = "UPDATE "+ this.#configurations.secondaryTable
-        + this.#UpdateValuesQueryHelper(extras,extras.length) 
-
-        this.#connection.exec(update_customer_extras_query)
-    }
-
-    #InsertValuesQueryHelper (attributes : any,length : number){
-        let attributesHelper = " ("
-        let valuesHelper = "Values("
-
-        for(let i = 0 ; i< length ; i++){
-            if(i < length-1){
-                valuesHelper += attributes[i].value + ","
-                attributesHelper += attributes[i].name +","
-            }
-            else{
-                valuesHelper += attributes[i].value  + ")"
-                attributesHelper += attributes[i].name + ") "
-            }
-        }
-        return attributesHelper + valuesHelper
-    }
-
-    #UpdateValuesQueryHelper(attributes : any,length : number){
-        let result = "Set "
-        for(let i = 0 ; i< length ; i++){
-            if(i < length-1){
-                result += attributes[i].name + "=" +attributes[i].value + ","
-            }
-            else{
-                result += attributes[i].name + "=" +attributes[i].value
-                result += " WHERE Id = " + attributes.id 
-            }
-        }
-        return result;
-    }
+import Database from "better-sqlite3"
 
 
-    async BanCustomer(customer : any){
-        let update_customer_banStatus_query = "UPDATE" + this.#configurations.secondaryTable
-            + "Set BanStatus = " + customer.banStatus + " WHERE Id = " + customer.id
+ 
+let configurations = {
+    databaseName : "customers.db",
+    databaseUrl : "./data",
+    mainTable : "Customers",
+    mainTableAttrbs : ["FullName","Email","PhoneNumber","BanStatus"],
+    secondaryTable : "Extras",
+    secondaryTableAttrbs : ["Rating","NegativeRating","Latitude","Longitude","Addresse"]
+}
 
-        this.#connection.exec(update_customer_banStatus_query)    
-    }
+let connection : Database.Database
 
-    async RateCustomer(customer : any){
-        let update_customer_banStatus_query = "UPDATE" + this.#configurations.secondaryTable
-        + "Set Rating = Rating + ${customer.rating}, NegativeRating = NegativeRating + ${customer.negativeRating} WHERE Id = ${customer.id}"
-        this.#connection.exec(update_customer_banStatus_query)    
-    }
 
-    async GetCustomerInfos(customerId : String){
-        let get_customer_infos = "SELECT "+ this.#GetValuesHelper("infos")
-            +" FROM "+this.#configurations.mainTable+"WHERE Id = "+ customerId
+async function connect(){
+    connection = new Database(configurations.databaseUrl + '/' +configurations.databaseName)
 
-        return await this.#connection.run(get_customer_infos)
+    const create_customers_table_query = "CREATE TABLE IF NOT EXISTS "+ configurations.mainTable +" (\n"
+    + "	Id String PRIMARY KEY,"
+    + "	FullName text NOT NULL,"
+    + "	Email text NOT NULL,"
+    + " PhoneNumber text NOT NULL,"
+    + " BanStatus bool DEFAULT true "
+    + ")"
 
-    }
+    const create_customers_extraInfos_table_query = "CREATE TABLE IF NOT EXISTS "+ configurations.secondaryTable +" ("
+    + "	Id String PRIMARY KEY,"
+    + " Rating real DEFAULT 0,"
+    + " NegativeRating real DEFAULT 0,"
+    + " Latitude real ,"
+    + " Longitude real ,"
+    + " Address text ,"
+    + ")"
 
-    async GetCustomerExtras(customerId : String){
-        let get_customer_extras = "SELECT "+ this.#GetValuesHelper("extras") 
-            +" FROM "+this.#configurations.secondaryTable+"WHERE Id = "+ customerId
-        return await this.#connection.run(get_customer_extras)
+    connection.prepare(create_customers_table_query).run()
+    connection.prepare(create_customers_extraInfos_table_query).run()
+}
 
-    }
+async function RegsiterCustomer(customer : any){
+    let register_customer_query = "INSERT INTO "+ configurations.mainTable
+        +InsertValuesQueryHelper(customer , customer.length)
 
-    #GetValuesHelper(type : String ){
-        let result = "("
-        const helper = (attributes : Array<string>)=>{
-            for (let i = 0 ; i < attributes.length ; i++){
-                if(i < length-1){
-                    result += attributes[i]
-                }
-                else{
-                    result += attributes[i] + ")"
-                }
-            }
-        }
-        if (type === "infos"){
-            helper(this.#configurations.mainTableAttrbs)
+    connection.prepare(register_customer_query).run()
+}
+
+async function RegisterCustomerExtras(extras : any){
+    let register_customer_extras_query = "INSERT INTO "+ configurations.secondaryTable
+    + InsertValuesQueryHelper(extras,extras.length) 
+
+    connection.prepare(register_customer_extras_query).run()
+}
+
+async function UpdateCustomer(customer : any){
+    let update_customer_query = "UPDATE "+ configurations.mainTable
+        +UpdateValuesQueryHelper(customer , customer.length)
+        
+
+    connection.prepare(update_customer_query).run()
+}
+
+async function UpdateCustomerExtras(extras : any){
+    let update_customer_extras_query = "UPDATE "+ configurations.secondaryTable
+    + UpdateValuesQueryHelper(extras,extras.length) 
+
+    connection.prepare(update_customer_extras_query).run()
+}
+
+function InsertValuesQueryHelper (attributes : any,length : number){
+    let attributesHelper = " ("
+    let valuesHelper = "Values("
+
+    for(let i = 0 ; i< length ; i++){
+        if(i < length-1){
+            valuesHelper += attributes[i].value + ","
+            attributesHelper += attributes[i].name +","
         }
         else{
-           helper(this.#configurations.secondaryTableAttrbs)
+            valuesHelper += attributes[i].value  + ")"
+            attributesHelper += attributes[i].name + ") "
         }
-        return result
     }
+    return attributesHelper + valuesHelper
+}
+
+function UpdateValuesQueryHelper(attributes : any,length : number){
+    let result = "Set "
+    for(let i = 0 ; i< length ; i++){
+        if(i < length-1){
+            result += attributes[i].name + "=" +attributes[i].value + ","
+        }
+        else{
+            result += attributes[i].name + "=" +attributes[i].value
+            result += " WHERE Id = " + attributes.id 
+        }
+    }
+    return result;
+}
+
+
+async function BanCustomer(customer : any){
+    let update_customer_banStatus_query = "UPDATE" + configurations.secondaryTable
+        + "Set BanStatus = " + customer.banStatus + " WHERE Id = " + customer.id
+
+    connection.prepare(update_customer_banStatus_query).run()   
+}
+
+async function RateCustomer(customer : any){
+    let update_customer_banStatus_query = "UPDATE" + configurations.secondaryTable
+    + "Set Rating = Rating + ${customer.rating}, NegativeRating = NegativeRating + ${customer.negativeRating} WHERE Id = ${customer.id}"
+    connection.prepare(update_customer_banStatus_query).run()
+}
+
+async function GetCustomerInfos(customerId : String){
+    let get_customer_infos = "SELECT "+ GetValuesHelper("infos")
+        +" FROM "+configurations.mainTable+"WHERE Id = "+ customerId
+
+    return await connection.prepare(get_customer_infos).get()
 
 }
 
-export default CustomersDatabase
+async function GetCustomerExtras(customerId : String){
+    let get_customer_extras = "SELECT "+ GetValuesHelper("extras") 
+        +" FROM "+configurations.secondaryTable+"WHERE Id = "+ customerId
+    return connection.prepare(get_customer_extras).get()
+
+}
+
+function GetValuesHelper(type : String ){
+    let result = "("
+    const helper = (attributes : Array<string>)=>{
+        for (let i = 0 ; i < attributes.length ; i++){
+            if(i < length-1){
+                result += attributes[i]
+            }
+            else{
+                result += attributes[i] + ")"
+            }
+        }
+    }
+    if (type === "infos"){
+        helper(configurations.mainTableAttrbs)
+    }
+    else{
+       helper(configurations.secondaryTableAttrbs)
+    }
+    return result
+}
+
+
+
