1de86c8471b87c62de26dc6f0a9e66765141ee2b
IDIRYACINE <idiryacinesp@gmail.com>
2022-04-18 17:22:38 +0100

    fixed heroku and ts-definitions

---
+++ src/App.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="node" />
+import http from 'http';
+declare function App(isTestMode: boolean, server: http.Server): void;
+export default App;
---
+++ src/Config.d.ts
@@ -0,0 +1,23 @@
+export declare const ProductsDatabaseConfig: {
+    databaseName: string;
+    databaseUrl: string;
+    categoryTableName: string;
+};
+export declare const CustomersDatabaseConfig: {
+    databaseName: string;
+    databaseUrl: string;
+    mainTable: string;
+    mainTableAttrbs: string[];
+    secondaryTable: string;
+    secondaryTableAttrbs: string[];
+};
+export declare const Emulator: {
+    firestore: string;
+    firestoreEnvKey: string;
+    database: string;
+    databaseEnvKey: string;
+    auth: string;
+    authEnvKey: string;
+    storage: string;
+    storageEnvKey: string;
+};
